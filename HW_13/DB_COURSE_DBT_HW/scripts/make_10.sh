5ba645e3eb7ca09db6ffb14a79b2129186f6d130
Pavlo Zhoholiev <p_zhoholiev@viyar.ua>
2025-09-03 06:59:07 +0300

    HW_13 Zhoholiev Pavlo - fix make_10.sh

--- HW_13/DB_COURSE_DBT_HW/scripts/make_10.sh
+++ HW_13/DB_COURSE_DBT_HW/scripts/make_10.sh
@@ -11,7 +11,7 @@ fi
 for file in *.csv; do
     if [ -f "$file" ]; then
         base_name=$(basename "$file" | cut -d. -f1)
-
+        makedir -p output
         head -n 10000 "$file" > "output/${base_name}_${ROWS}.csv"
         echo "Processed: $file -> output/${base_name}_${ROWS}.csv"
     fi
